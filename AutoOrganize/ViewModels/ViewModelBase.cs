097895571b79227ba8cd6de95e806238bae37dd2
hatulaile <hatulaile@outlook.com>
2026-02-02 22:08:47 +0800

    初始提交

---
+++ AutoOrganize/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AutoOrganize.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ AutoOrganize/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AutoOrganize.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
